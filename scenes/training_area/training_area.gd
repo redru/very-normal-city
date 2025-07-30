1c5e8222e7a54fa56af184ee38e2bc40867d288f
Luca <redru@users.noreply.github.com>
2025-07-30 22:55:19 +0200

    Remove unused method

--- scenes/training_area/training_area.gd
+++ scenes/training_area/training_area.gd
@@ -5,9 +5,6 @@ const quest_id: int = 0
 @onready var shape = $CollisionShape3D
 @onready var mesh = $MeshInstance3D
 
-func _ready() -> void:
-	pass
-
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
 		visible = false
